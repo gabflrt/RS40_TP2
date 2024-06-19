efd67e18c1065c07bfb1222b4e5cffb790b48fc3
Gabriel Fleuret <gabfleuret@gmail.com>
2024-06-19 20:56:00 +0200

    add user connection

--- test/TP2/run_server.py
+++ test/TP2/run_server.py
@@ -7,6 +7,9 @@ Created on May 2022
 """
 
 from flask import Flask
+from flask import request
+from flask import render_template
+import hashlib  
 
 HTTPS_MODE = True
 
@@ -14,10 +17,31 @@ HTTPS_MODE = True
 SECRET_MESSAGE = "aziz" # A modifier
 app = Flask(__name__)
 
+@app.route('/', methods=['GET', 'POST'])
 
-@app.route("/")
 def get_secret_message():
-    return SECRET_MESSAGE
+    if request.method == 'POST':
+        # Reception des donnees du formulaire
+        userName = request.form['userName']
+        password = request.form['password']
+        #hash du mot de passe en entré pour obtenir le même résultat
+        inputPassword = hashlib.sha256(password.encode()).hexdigest()
+        # Verification du login. 
+        # Si correct -> redirection vers page.html
+        # Sinon redirection vers la page login.html avec message d'erreur
+        if userName == "user" and inputPassword == mdpHasher:
+            return render_template('accueil.html', userName=userName, message=
+    SECRET_MESSAGE)
+        else:
+            error = 'Oups! Votre login ou mot de passe est incorrect. Veuillez réessayer.'
+            return render_template('index.html', error=error)
+    else:
+        return render_template('index.html')
+
+#definition du mot de passe
+userPassword = "mdp"
+#hash du mot de passe avec SHA256 pour plus de sécurité
+mdpHasher = hashlib.sha256(userPassword.encode()).hexdigest()
 
 
 if __name__ == "__main__":
@@ -33,5 +57,5 @@ if __name__ == "__main__":
         # HTTPS version
         # A compléter  : nécessité de déplacer les bons fichiers vers ce répertoire
         context = ("resources/server-public-key.pem", "resources/server-private-key.pem")
-        app.run(debug=True, host="0.0.0.0", port=8081, ssl_context=context)
+        app.run(debug=True, host="127.0.0.1", port=8081, ssl_context=context)
    
